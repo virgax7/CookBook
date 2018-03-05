bb341966d4b4f97b15c0fb9090647c45e4044fb2
pyoung <pyoungx7@gmail.com>
2018-03-04 20:12:05 -0500

    moved things over to react as client and express as backend

--- app.js
+++ app.js
@@ -11,33 +11,38 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(favicon(path.join(__dirname, "public", "food.ico")));
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/users", (req, res) => {
+    res.json([{
+        id: 1,
+        username: "pyoung"
+    }, {
+        id: 2,
+        username: "richard"
+    }])
 });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  let err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
