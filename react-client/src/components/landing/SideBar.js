96e2f0a3c0f9eed8eede92be5cd079dffd9f9e4e
pyoung <pyoungx7@gmail.com>
2018-03-07 07:10:10 -0500

    basic hardcoded sidebar float

--- react-client/src/components/landing/SideBar.js
+++ react-client/src/components/landing/SideBar.js
@@ -4,7 +4,16 @@ class SideBar extends Component {
     render() {
         return (
             <div id="sideBar">
-                <h1 class="sideBarContents">SideBar</h1>
+                <h1 id="categoryTitle">Category</h1>
+                <ul class="categories">
+                    {/*just some mock data for now...*/}
+                    <li class="categoryContent">On the Go</li>
+                    <li class="categoryContent">Family</li>
+                    <li class="categoryContent">Health Friendly</li>
+                    <li class="categoryContent">Italian</li>
+                    <li class="categoryContent">Chinese</li>
+                    <li class="categoryContent">Romantic</li>
+                </ul>
             </div>
         );
     }
