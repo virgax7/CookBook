d13c1db8da482d2fa7b6f4802b51e5f40a63a1b5
pyoung <pyoungx7@gmail.com>
2018-03-07 07:42:07 -0500

    basic main content layout

--- react-client/src/components/landing/MainContent.js
+++ react-client/src/components/landing/MainContent.js
@@ -1,10 +1,35 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class MainContent extends Component {
     render() {
         return (
             <div id="mainContent">
-                <h1>Main Content</h1>
+                <div id="choices">Choices</div>
+                {/*hard coded images, later retrieve from db or something*/}
+                <div class="gallery">
+                    <img src={require('../../images/landing/burrito.jpeg')} alt="burrito" width="400" height="300"/>
+                    <div class="desc">Delicious Burrito</div>
+                </div>
+                <div class="gallery">
+                    <img src={require('../../images/landing/pizza.jpeg')} alt="burrito" width="400" height="300"/>
+                    <div class="desc">Combination Pizza</div>
+                </div>
+                <div class="gallery">
+                    <img src={require('../../images/landing/salmon.jpeg')} alt="burrito" width="400" height="300"/>
+                    <div class="desc">Fresh Salmon</div>
+                </div>
+                <div class="gallery">
+                    <img src={require('../../images/landing/shrimp.jpeg')} alt="burrito" width="400" height="300"/>
+                    <div class="desc">Authentic shrimp</div>
+                </div>
+                <div class="gallery">
+                    <img src={require('../../images/landing/stir-fry.jpeg')} alt="burrito" width="400" height="300"/>
+                    <div class="desc">Grandma's Stir fry</div>
+                </div>
+                <div class="gallery">
+                    <img src={require('../../images/landing/pancake.jpeg')} alt="burrito" width="400" height="300"/>
+                    <div class="desc">Mouth watering pancakes</div>
+                </div>
             </div>
         );
     }
