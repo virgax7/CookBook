a5fcd129ad53a64883f1b0e3b4afc910de9dff77
pyoung <pyoungx7@gmail.com>
2018-03-13 01:12:19 -0400

    hooked up react routing

--- react-client/src/containers/app/App.js
+++ react-client/src/containers/app/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
+import {Route, NavLink, HashRouter} from "react-router-dom";
+
 import "../reset.css";
 import "./App.css";
+
 import NavBar from "../../components/common/NavBar";
-import SideBar from "../../components/landing/SideBar";
-import MainContent from "../../components/landing/MainContent";
 import Footer from "../../components/common/Footer";
+import SurpriseMe from "../../components/surprise/SurpriseMe";
+import {Landing} from "../../components/landing/Landing";
 
 class App extends Component {
     constructor(props) {
@@ -13,15 +16,35 @@ class App extends Component {
 
     render() {
         return (
-            <div id="mainWrapper">
-                <NavBar/>
-                <div className="spacer"></div>
-                <div id="sideBarWrapper">
-                    <SideBar/>
-                    <MainContent/>
+            <HashRouter>
+                <div id="mainWrapper">
+                    {/*<NavBar/>*/}
+                    {/*--start tmp replacement for navbar*/}
+                    <div id="navBar">
+                        <a className="navBarContents" id="cookBookTitle">
+                            <NavLink className="navBarLink" to="/">CookBook</NavLink>
+                        </a>
+                        <a href="#" className="navBarContents">Trending</a>
+                        <a href="#" className="navBarContents">Contact</a>
+                        <a className="navBarContents">
+                            <NavLink className="navBarLink" to="/surpriseMe">Surprise Me</NavLink>
+                        </a>
+                        <div id="searchWrapper">
+                            <form>
+                                <input type="text" placeholder="Search.." name="search" id="searchInputText"/>
+                                <button type="submit" id="searchSubmitButton"/>
+                            </form>
+                        </div>
+                    </div>
+                    {/*--end tmp replacement for navbar*/}
+                    <div className="spacer"></div>
+                    <div className="content">
+                        <Route exact path="/" component={Landing} />
+                        <Route path="/surpriseMe" component={SurpriseMe} />
+                    </div>
+                    <Footer/>
                 </div>
-                <Footer />
-            </div>
+            </HashRouter>
         );
     }
 }
