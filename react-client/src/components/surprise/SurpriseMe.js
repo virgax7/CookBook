789ba61773577fce4efe7db1e4733d12dd64ea85
pyoung <pyoungx7@gmail.com>
2018-03-13 01:12:39 -0400

    added surprise me page for react routing

---
+++ react-client/src/components/surprise/SurpriseMe.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class SurpriseMe extends Component {
+    render() {
+        return (
+            <div id="mainWrapper">
+                <div>Surprise page</div>
+            </div>
+        );
+    }
+}
+
+export default SurpriseMe;
