04827585a103462e648674e6989832ea7aacd83c
pyoung <pyoungx7@gmail.com>
2018-03-17 19:24:44 -0400

    fixed unused import and nested a tag elems

--- react-client/src/components/common/NavBar.js
+++ react-client/src/components/common/NavBar.js
@@ -3,18 +3,10 @@ import {NavLink} from "react-router-dom";
 
 export const NavBar = () => (
         <div id="navBar">
-            <a className="navBarContents" id="cookBookTitle">
-                <NavLink className="navBarLink" exact to="/">CookBook</NavLink>
-            </a>
-            <a className="navBarContents">
-                <NavLink className="navBarLink" to="/trending">Trending</NavLink>
-            </a>
-            <a className="navBarContents">
-                <NavLink className="navBarLink" to="/contact">Contact</NavLink>
-            </a>
-            <a className="navBarContents">
-                <NavLink className="navBarLink" to="/surpriseMe">Surprise Me</NavLink>
-            </a>
+            <NavLink className="navBarContents navBarLink" id={"cookBookTitle"}exact to="/">CookBook</NavLink>
+            <NavLink className="navBarContents navBarLink" to="/trending">Trending</NavLink>
+            <NavLink className="navBarContents navBarLink" to="/contact">Contact</NavLink>
+            <NavLink className="navBarContents navBarLink" to="/surpriseMe">Surprise Me</NavLink>
             <div id="searchWrapper">
                 <form>
                     <input type="text" placeholder="Search.." name="search" id="searchInputText"/>
