050d5259fa5aca22c9448d09fc522dfb30739b35
pyoung <pyoungx7@gmail.com>
2018-03-18 23:03:26 -0400

    search bar now searches firebase for strings including the search val

--- react-client/src/components/cook/SimCookSearch.js
+++ react-client/src/components/cook/SimCookSearch.js
@@ -1,14 +1,43 @@
 import React, {Component} from "react";
 import "./cook.css";
+import * as firebase from "firebase";
 
 export default class SimCookSearch extends Component {
     constructor(props) {
         super(props);
         this.handleStart = this.handleStart.bind(this);
+        this.updateSearchValue = this.updateSearchValue.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            searchValue: ""
+        };
     }
 
     handleStart() {
         this.props.onStartKitchen("Mac", [], [], []);
+    }
+
+    updateSearchValue(event) {
+        this.setState({
+           searchValue: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        if (this.state.searchValue === "") {
+            return;
+        }
+        event.preventDefault();
+        const searchResults = [];
+        firebase.database().ref("Recipes").on("value", snap => {
+            snap.forEach(user => {
+                user.forEach(recipe => {
+                    if (recipe.key.toUpperCase().includes(this.state.searchValue.toUpperCase())) {
+                        searchResults.push(<li className="searchContent" key={recipe.key}>{recipe.key}</li>);
+                    }
+                });
+            });
+        });
     }
 
     render() {
@@ -16,8 +45,8 @@ export default class SimCookSearch extends Component {
             <div id={"simCookSearch"}>
                 <div id="searchSpacer"/>
                 <div id={"searchDiv"} className={"center"}>
-                    <form id={"searchForm"} className={"center"}>
-                        <input className={"center"} type={"text"} placeholder={"Search..."} name={"search"}/>
+                    <form id={"searchForm"} className={"center"} onSubmit={this.handleSubmit}>
+                        <input className={"center"} type={"text"} placeholder={"Search..."} name={"search"} onChange={this.updateSearchValue}/>
                         <button className={"center"} type={"submit"}>Submit</button>
                     </form>
                 </div>
