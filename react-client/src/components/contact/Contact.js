87491621afc44275cbd1b24465177b10e01347a3
Richarder <zhangruiqian5@gmail.com>
2018-03-24 16:06:35 -0400

    updated contact page

--- react-client/src/components/contact/Contact.js
+++ react-client/src/components/contact/Contact.js
@@ -1,10 +1,48 @@
 import React, {Component} from "react";
+import * as firebase from "firebase";
 
-const Contact = () => (
-    <div>
-        <h1>Pyoung Kang Kim, Address: In the shores of lake amayo</h1>
-        <h1>Richard Zhang, Address: You have to fill this out bruh</h1>
-    </div>
-);
+class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+            contacts: []
+        }
+    }
+
+    componentWillMount() {
+        const contactListRef = firebase.database().ref("ContactList");
+        contactListRef.on("value", contactList => {
+            const contactArr = [];
+            let map = new Map();
+            contactList.forEach(contact => {
+                map.set(contact.key, []);
+                contact.forEach(contactInfo => {
+                    map.get(contact.key).push(contactInfo.key + ": " + contactInfo.val());
+                });
+            });
+            // console.log(map);
+            map.forEach((val, key) => {
+                contactArr.push(<div className={"contacts"} key={key}>{key}</div>);
+                val.forEach(value => {
+                    contactArr.push(<div className={"contact"} key={value}>{value}</div>);
+                })
+            });
+            this.setState({
+                contacts: contactArr
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div id="Contact">
+                <div id="contactTitle">Contacts</div>
+                <div className="contacts">
+                    {this.state.contacts}
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Contact;
